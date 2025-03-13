deaf9594463ebac2753e62f079cc50936660555f
kaitlincastor <kncastor@gmail.com>
2025-03-13 11:07:45 -0700

    safety test completed and working

--- tests/safety_test.R
+++ tests/safety_test.R
@@ -1,7 +1,8 @@
+#testing the function works
+
+expected <- list(prediction = 1, message = "According to our model the water is not safe to swim in.")
 
 test_that("safety-function_works", {
-  
-  expect_equal(predict_safety(flow_volume = 20, stage = 3), 1)
-  expect_equal(predict_safety(flow_volume = 20, stage = 3), 
-               "According to our model the water is not safe to swim in.")
+  expect_equal(predict_safety(flow_volume = 20, stage = 3), expected)
+
 })
